ca5ce4e1ea4012352bb9b34711802078b5ff5bbf
DollarboySAGE <mightyboss18@gmail.com>
2022-10-24 18:58:50 +0100

    added js file

---
+++ App.js
@@ -0,0 +1,6 @@
+const milliSeconds = document.querySelector(".milsec")
+const seconds = document.querySelector(".seconds")
+const minute = document.querySelector(".min")
+const hour = document.querySelector(".hour")
+
+const pauseBtn = document.querySelector(".")
